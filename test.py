bb02c45f223c26b68fe0c3478a7b57cfb4d47c4d
marwan ahmed <marwanmedo3free@gmail.com>
2026-09-21 15:15:04 +0300

    project done

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
-res=search_flights("plan a 7 days japan trip from Bangladesh")
-print(res)
+user_input=input("enter travel requests: ")
+
+res=run_travel_agent(user_input=user_input,thread_id="test_user")
+print("\nFinal response:\n")
+print(res["answer"])
+
+# res=search_flights("plan a 7 days japan trip from Bangladesh")
+# print(res)
 
 # res=tavily_search("best hotels in egypt")
 # print(res)
