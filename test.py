ef21ff350dd05c2da73cdb94d80a8f8385a5d363
marwan ahmed <marwanmedo3free@gmail.com>
2026-09-22 14:17:37 +0300

    done finally

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-from tools.tavily_tool import tavily_search
-from tools.flight_tool import search_flights
-from backend import run_travel_agent
+import asyncio
+from mcp_client import get_all_tools
 
-user_input=input("enter travel requests: ")
-
-res=run_travel_agent(user_input=user_input,thread_id="test_user")
-print("\nFinal response:\n")
-print(res["answer"])
-
-# res=search_flights("plan a 7 days japan trip from Bangladesh")
-# print(res)
-
-# res=tavily_search("best hotels in egypt")
-# print(res)
+if __name__=="__main__":
+    asyncio.run(get_all_tools())
